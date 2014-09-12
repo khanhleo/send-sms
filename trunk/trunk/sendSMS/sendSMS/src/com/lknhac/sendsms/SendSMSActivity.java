6a71d0663e60663c97d93019660cbf5cc6bd4a27
lekhanhnhac91@gmail.com <lekhanhnhac91@gmail.com@d72e19e1-b254-4e8b-b93c-184d03725891>
2014-09-12 10:04:14 +0000

    - commit 12-9

--- trunk/trunk/sendSMS/sendSMS/src/com/lknhac/sendsms/SendSMSActivity.java
+++ trunk/trunk/sendSMS/sendSMS/src/com/lknhac/sendsms/SendSMSActivity.java
@@ -1,5 +1,6 @@
 package com.lknhac.sendsms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
@@ -16,10 +17,11 @@ import com.lknhac.sendsms.data.PhoneListDao;
 import com.lknhac.sendsms.object.ContactItem;
 
 public class SendSMSActivity extends Activity {
-	
+
 	private static final String TAG = "SendSMSActivity";
 	private EditText txt_content, txt_from, txt_to;
 	private List<ContactItem> listSummary;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -27,65 +29,73 @@ public class SendSMSActivity extends Activity {
 		setContentView(R.layout.send_sms);
 		init();
 	}
-	
-	public void clickSend(View view){
+
+	public void clickSend(View view) {
 		sendData();
 	}
-	
-	private void sendData(){
-		String number ="";
+
+	private void sendData() {
+		String number = "";
 		PhoneListDao summaryDao = new PhoneListDao(SendSMSActivity.this);
-		listSummary = summaryDao.selectAll();
-//		SmsManager smsManager = SmsManager.getDefault();
-//		smsManager
-//				.sendTextMessage(
-//						listSummary.get(0).getPhoneNumber().toString(),
-//						null,
-//						txt_content.getText().toString(),
-//						null, null);
-//		Log.d(TAG,listSummary.get(0).getPhoneNumber() +": "+txt_content.getText().toString());
-		for (ContactItem item : listSummary) {
-//			ContentValues values = new ContentValues();
-//			values.put("address", item.getPhoneNumber());// sender name
-//			values.put("body", txt_content.toString());
-//			getContentResolver().insert(Uri.parse("content://sms/inbox"),
-//					values);
-			
-//			SmsManager smsManager = SmsManager.getDefault();
-//			smsManager
-//					.sendTextMessage(
-//							item.getPhoneNumber(),
-//							null,
-//							txt_content.getText().toString(),
-//							null, null);
-//			Log.d(TAG,item.getPhoneNumber());
-			number+= item.getPhoneNumber()+";";
+		listSummary = summaryDao.selectForClassDetails("6.1");
+		try {
+			SmsManager smsManager = SmsManager.getDefault();
+			ArrayList<String> parts = smsManager.divideMessage("Nguyễn Văn A Lớp 2.1 Toán:9 Văn:9 Tiếng Việt:6 Thể dục:10 Sức khỏe:8 Công dân:9");
+			smsManager.sendMultipartTextMessage("01228760657", null, parts, null, null);
+//			smsManager.sendTextMessage("01228760657", null, , null,
+//					null);
+			Log.v(TAG, "SMS Sent!");
+		} catch (Exception e) {
+			e.printStackTrace();
+			Log.v(TAG, "SMS faild");
 		}
-		
+		Log.d(TAG, listSummary.get(0).getPhoneNumber() + ": "
+				+ txt_content.getText().toString());
+		for (ContactItem item : listSummary) {
+			// ContentValues values = new ContentValues();
+			// values.put("address", item.getPhoneNumber());// sender name
+			// values.put("body", txt_content.toString());
+			// getContentResolver().insert(Uri.parse("content://sms/inbox"),
+			// values);
+
+			// SmsManager smsManager = SmsManager.getDefault();
+			// smsManager
+			// .sendTextMessage(
+			// item.getPhoneNumber(),
+			// null,
+			// txt_content.getText().toString(),
+			// null, null);
+			// Log.d(TAG,item.getPhoneNumber());
+			number += item.getPhoneNumber() + ";";
+		}
+
 		summaryDao.close();
-		sendSMS(number);
+		// sendSMS(number);
 	}
-	 protected void sendSMS(String number) {
-	      Log.i("Send SMS", "");
 
-	      Intent smsIntent = new Intent(Intent.ACTION_VIEW);
-	      smsIntent.setData(Uri.parse("smsto:"));
-	      smsIntent.setType("vnd.android-dir/mms-sms");
+	protected void sendSMS(String number) {
+		Log.i("Send SMS", "");
 
-	      smsIntent.putExtra("address"  , number);
-	      smsIntent.putExtra("sms_body"  ,txt_content.getText().toString());
-	      try {
-	         startActivity(smsIntent);
-	         finish();
-	         Log.i("Finished sending SMS...", "");
-	      } catch (android.content.ActivityNotFoundException ex) {
-	         Toast.makeText(SendSMSActivity.this, 
-	         "SMS faild, please try again later.", Toast.LENGTH_SHORT).show();
-	      }
-	   }
-	private void init(){
+		Intent smsIntent = new Intent(Intent.ACTION_VIEW);
+		smsIntent.setData(Uri.parse("smsto:"));
+		smsIntent.setType("vnd.android-dir/mms-sms");
+
+		smsIntent.putExtra("address", number);
+		smsIntent.putExtra("sms_body", txt_content.getText().toString());
+		try {
+			startActivity(smsIntent);
+			finish();
+			Log.i("Finished sending SMS...", "");
+		} catch (android.content.ActivityNotFoundException ex) {
+			Toast.makeText(SendSMSActivity.this,
+					"SMS faild, please try again later.", Toast.LENGTH_SHORT)
+					.show();
+		}
+	}
+
+	private void init() {
 		txt_content = (EditText) findViewById(R.id.txt_content);
-		txt_from =(EditText) findViewById(R.id.txt_from);
+		txt_from = (EditText) findViewById(R.id.txt_from);
 		txt_to = (EditText) findViewById(R.id.txt_to);
 	}
 
